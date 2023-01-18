3c3bd34b68958af8e9ac60ffb1a1fa8e922b04de
kirtanchandak <themarketingkirtan@gmail.com>
2023-01-18 11:58:58 +0530

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Add more here
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
